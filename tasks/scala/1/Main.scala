424dc98b9e8c476c8e44347a4048504d4543b2c4
ruabininAD <ryabinin.ad@edu.spbstu.ru>
2023-11-26 14:08:16 +0300

    solve

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -9,20 +9,70 @@ object Main {
         print(pascal(col, row) + " ")
       println()
     }
+
+//    println("Exercise 2 Parentheses Balancing")
+//    val listOfLists = List(
+//      List('(', ')', '(', ')', '(', ')', '(', ')', '(', ')'),
+//      List('(', '(', '(', '(', ')', ')', ')', ')', ')', '(', ')'),
+//      List('(', '(', '('),
+//      List('('),
+//      List(')'),
+//      List(')', ')', ')'),
+//      List('(', ')', '('),
+//      List(')', '(', ')'),
+//      List('(', ')'),
+//      List('(', ')', '(', ')', '(', ')'),
+//      List('(', '(', '(', ')', ')', ')')
+//    )
+//    for (lst <- listOfLists) {
+//      println(lst.toString(), balance(lst))
+//    }
+//    println()
+//
+//
+//    println()
+//    val coinsList = List(
+//      List(1, 2, 4, 5),
+//      List(2, 4, 6, 8),
+//      List(1, 3, 4, 5),
+//      List(3, 5, 6, 7),
+//    )
+//
+//    val targetList = List(
+//      9, 11, 4, 6
+//    )
+//
+//    for (target <- targetList; coins <- coinsList) {
+//      println(s" target=$target, coins=$coins countChange() =  ${countChange(target, coins)}")
+//    }
+
+
+
   }
 
   /**
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    (c, r) match {
+      case (1,1) => 1
+      case (_, _)if c < 1 || c > r => 0
+      case (c, r) =>  pascal(c-1, r-1)+pascal(c, r-1)
+    }
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
+
+  def help_balance(count: Int, chars: List[Char]): Boolean = chars match {
+    case _ if count < 0 => false
+    case '(' :: tail => help_balance(count + 1, tail)
+    case ')' :: tail => help_balance(count - 1, tail)
+    case Nil => count == 0
+  }
   def balance(chars: List[Char]): Boolean = {
-   
+    help_balance(0, chars)
   }
 
   /**
@@ -33,6 +83,24 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
+    def combinations(list: List[Int]): List[List[Int]] = list match {
 
+      case Nil => List(Nil)
+      case head :: tail =>
+        val tailCombinations = combinations(tail)
+        tailCombinations ++ tailCombinations.map(head :: _)
+    }
+
+
+    def sumHelper(acc: Int, target: Int, lst: List[Int]): Boolean = {
+      lst match {
+        case Nil => acc == target
+        case head :: tail => sumHelper( acc + head,target, tail)
+      }
+    }
+
+    val pred: List[Int] => Boolean = lst => sumHelper(0, money, lst)
+    combinations(coins).count(pred)
   }
+
 }
--- tasks/scala/2/src/main/scala/funsets/Main.scala
+++ tasks/scala/2/src/main/scala/funsets/Main.scala
@@ -4,3 +4,57 @@ object Main extends App {
   import FunSets._
   println(contains(singletonSet(1), 1))
 }
+
+//
+//object Main extends App {
+//
+//
+//  //  тестовые множества
+//  val set1: funsets.FunSets.Set = funsets.FunSets.singletonSet(1)
+//  val set2: funsets.FunSets.Set = funsets.FunSets.singletonSet(2)
+//  val set3: funsets.FunSets.Set = funsets.FunSets.singletonSet(3)
+//  val set4: funsets.FunSets.Set = funsets.FunSets.singletonSet(4)
+//  val set5: funsets.FunSets.Set = funsets.FunSets.singletonSet(5)
+//
+//
+//  println("set1 = " + funsets.FunSets.toString(set1))
+//  println("set2 = " + funsets.FunSets.toString(set2))
+//  println("set3 = " + funsets.FunSets.toString(set3))
+//  println("set4 = " + funsets.FunSets.toString(set4))
+//  println("set5 = " + funsets.FunSets.toString(set5))
+//
+//  val unionSet: funsets.FunSets.Set =   funsets.FunSets.union(set3, funsets.FunSets.union(set1, set2))
+//  println("Union of set1, set2, set3 (unionSet1) = " + funsets.FunSets.toString(unionSet))
+//
+//  val unionSet2: funsets.FunSets.Set = funsets.FunSets.union(set5, funsets.FunSets.union(set4, funsets.FunSets.union(set3,  set2)))
+//  println("Union of  set2, set3,set4,set5, (unionSet2) = " + funsets.FunSets.toString(unionSet2))
+//
+//  val intersectSet: funsets.FunSets.Set = funsets.FunSets.intersect(unionSet2, unionSet)
+//  println("Intersection of unionSet and unionSet2 = " + funsets.FunSets.toString(intersectSet))
+//
+//  val diffSet: funsets.FunSets.Set = funsets.FunSets.diff(unionSet, set2)
+//  println("Difference of unionSet and set2 = " + funsets.FunSets.toString(diffSet))
+//
+//  val filteredSet: funsets.FunSets.Set = funsets.FunSets.filter(unionSet, (x: Int) => x > 1)
+//  println("Filtered set (elements > 1) = " + funsets.FunSets.toString(filteredSet))
+//
+//  val filteredSet2: funsets.FunSets.Set = funsets.FunSets.filter(unionSet2, (x: Int) => x < 1)
+//  println("Filtered unionSet2 (elements < 1) = " + funsets.FunSets.toString(filteredSet2))
+//
+//
+//  val f: Int => Int = (x: Int) => x * 2
+//  val mappedSet: funsets.FunSets.Set = funsets.FunSets.map(unionSet, f)
+//  println("Mapped unionSet (each element multiplied by 2) = " + funsets.FunSets.toString(mappedSet))
+//
+//
+//
+//
+//
+//  println("Forall elements in unionSet1 > 2: " + funsets.FunSets.forall(unionSet, (x: Int) => x > 2))
+//  println("Forall elements in unionSet1 > 0: " + funsets.FunSets.forall(unionSet, (x: Int) => x > 0))
+//  println()
+//  println("Exists element in unionSet1 == 1: " + funsets.FunSets.exists(unionSet, (x: Int) => x == 1))
+//  println("Exists element in unionSet1 == 2: " + funsets.FunSets.exists(unionSet, (x: Int) => x == 2))
+//  println("Exists element in unionSet1 == 3: " + funsets.FunSets.exists(unionSet, (x: Int) => x == 3))
+//  println("Exists element in unionSet1 == 4: " + funsets.FunSets.exists(unionSet, (x: Int) => x == 4))
+//}
