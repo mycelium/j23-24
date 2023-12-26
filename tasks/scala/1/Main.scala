639f949d4f3aaa0def05685bd4868022e8228921
marg0sav <margo@savenko.su>
2023-12-26 23:14:01 +0300

    Done tasks one and two.

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -2,8 +2,8 @@ package recfun
 import common._
 
 object Main {
-  def main(args: Array[String]) {
-    println("Pascal's Triangle")
+  def main(args: Array[String]): Unit = {
+    println("Треугольник Паскаля")
     for (row <- 0 to 10) {
       for (col <- 0 to row)
         print(pascal(col, row) + " ")
@@ -11,19 +11,46 @@ object Main {
     }
   }
 
+  val balancedExpression = "(if (a>0) 1 else (0))"
+  val unbalancedExpression = "(()()))"
+
+  val balancedResult = balance(balancedExpression.toList)
+  val unbalancedResult = balance(unbalancedExpression.toList)
+
+  println(s"Результат сбалансированного выражения: $balancedResult")
+  println(s"Результат несбалансированоого выражения: $unbalancedResult")
+
+
+  val coins1 = List(1, 2, 3,4,5)
+  val ways1 = countChange(5, coins1)
+  println(s"Количество способов разменять 5 монетами номиналом $coins1: $ways1")
+
+  val coins2 = List(2, 3)
+  val ways2 = countChange(5, coins2)
+  println(s"Количество способов разменять 5 монетами номиналом $coins2: $ways2")
+
+
   /**
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if (c == 0 || c == r) 1 else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
+
 
   /**
    * Exercise 2 Parentheses Balancing
    */
+
   def balance(chars: List[Char]): Boolean = {
-   
+      chars.foldLeft(0)((count, char) =>
+        if (count < 0) count
+        else if (char == '(') count + 1
+        else if (char == ')') count - 1
+        else count
+      ) == 0
   }
+
 
   /**
    * Exercise 3 Counting Change
@@ -32,7 +59,10 @@ object Main {
    * there is 1 way to give change for 5 if you have coins with denomiation
    * 2 and 3: 2+3.
    */
-  def countChange(money: Int, coins: List[Int]): Int = {
 
+  def countChange(money: Int, coins: List[Int]): Int = {
+      if (money == 0) 1
+      else if (money < 0 || coins.isEmpty) 0
+      else countChange(money - coins.head, coins) + countChange(money, coins.tail)
   }
 }
--- tasks/scala/2/src/main/scala/funsets/Main.scala
+++ tasks/scala/2/src/main/scala/funsets/Main.scala
@@ -3,4 +3,52 @@ package funsets
 object Main extends App {
   import FunSets._
   println(contains(singletonSet(1), 1))
+  val set1: Set = singletonSet(1)
+  val set2: Set = singletonSet(2)
+  val set3: Set = singletonSet(3)
+
+  val unionSet: Set = union(set1, set2)
+  println("Union")
+  println("unionSet")
+  printSet(unionSet)
+  println(s"The unionSet contains 1:${contains(unionSet, 1)}")
+  println(s"The unionSet contains 2:${contains(unionSet, 2)}")
+
+
+  val intersectionSet: Set = intersect(unionSet,set1)
+  val intersectionSet1: Set = intersect(unionSet,set3)
+  println("Intersection")
+  println("intersectionSet")
+  printSet(intersectionSet)
+  println("intersectionSet1")
+  printSet(intersectionSet1)
+  println(s"The intersectionSet contains 1: ${contains(intersectionSet, 1)}")
+  println(s"The intersectionSet contains 2:${contains(intersectionSet, 2)}")
+  println(s"The intersectionSet1 contains 3:${contains(intersectionSet1, 1)}")
+
+  println("Filter")
+  val filteredSet: Set = filter(unionSet, (x: Int) => x > 1)
+  println("filteredSet with a condition x > 1 applied to the set unionSet")
+  printSet(filteredSet)
+  val unionSet1: Set = union(unionSet, set3)
+  println("unionSet1")
+  printSet(unionSet1)
+  val filteredSet1: Set = filter(unionSet1, (x: Int) => x > 1)
+  println("filteredSet1 with a condition x > 1 applied to the set unionSet1")
+  printSet(filteredSet1)
+
+  println("forall")
+  val result: Boolean = forall(unionSet1, (x: Int) => x > 0)
+  println(s"forall with a condition x > 0 for set unionSet1: $result")
+  val result1: Boolean = forall(unionSet1, (x: Int) => x < 0)
+  println(s"forall with a condition x < 0 for set unionSet1: $result1")
+
+  println("exists")
+  println(s"exists with a condition x > 2 for set unionSet1: ${exists(unionSet1, (x: Int) => x > 2)}")
+  println(s"exists with a condition x < 0 for set unionSet1: ${exists(unionSet1, (x: Int) => x < 0)}")
+
+  println("map")
+  val mappedSet: Set = map(unionSet1, (x: Int) => x * 2)
+  println("new set mappedSet after applying the condition x * 2 to unionSet1")
+  printSet(mappedSet)
 }
